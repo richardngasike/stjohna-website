4b02c4ce61d2176c623c9084c492e64ac782cf49
Richard Ngasike <107096899+richardngasike@users.noreply.github.com>
2025-08-01 22:51:44 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-};
-
-export default nextConfig;
-
-export default {
   images: {
     unoptimized: true,
   },
 };
+
+export default nextConfig;
