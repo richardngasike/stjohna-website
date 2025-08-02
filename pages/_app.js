21862c04825a912a596ce8974c95dec5544c03aa
Richard Ngasike <107096899+richardngasike@users.noreply.github.com>
2025-08-02 14:55:03 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import WhatsAppFAB from '@/components/WhatsAppFAB';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <WhatsAppFAB />
+    </>
+  );
 }
