7a94d87fd7c4219986fc0c27ddb9e7a3e9792c66
Richard Ngasike <107096899+richardngasike@users.noreply.github.com>
2025-09-02 13:29:15 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  // output: 'export',
 };
 
 export default nextConfig;
+
